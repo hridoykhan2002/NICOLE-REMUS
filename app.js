de84040a9023acb000bfe6e17955cabe82ef1ac7
Rabbi Raj HRidoy <114390390+hridoykhan2002@users.noreply.github.com>
2022-12-14 14:24:25 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+// navabr
+const togBtn = document.querySelector('.togBtn');
+const navLink = document.querySelector('.link');
+
+togBtn.addEventListener('click',() =>{
+    navLink.classList.toggle("show-nav")
+})
